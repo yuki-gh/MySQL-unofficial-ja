6b1248cdd6a2062c7ebe7bb039aa2e9b4520295d
Takayuki Ito <yuki@euc.jp>
2016-08-10 22:38:43 +0900

    fix

--- install_win.bat
+++ install_win.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 
-set dir="MySQL\MySQL Workbench 6.3 CE\data"
+set dir=MySQL\MySQL Workbench 6.3 CE\data
 copy /-y Workbench\*.xml "%ProgramFiles%\%dir%"
 copy /-y Workbench\*.xml "%ProgramFiles(x86)%\%dir%"
